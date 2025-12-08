mattmatterson111/IS12-Warfare-Two

Bayonet Charges (#132)
* step 1 - vars

* step 2: update those vars

* Finish adding attack in a sec

* TALLY HO LADS

* this is working

* Balancing

* polishing

* Prevent potential cheese

code/_onclick/item_attack.dm
@@ -87,14 +87,20 @@ avoid code duplication. This includes items that may sometimes act as a standard
 			to_chat(user, "<span class='warning'>The [src] is not ready to attack again!</span>")
 		return 0
 
-	if(!user.combat_mode)
+	if(!user.combat_mode && !user.TALLYHOLADS)
 		special = FALSE
 
 	if(M == user)//Hitting yourself.
 		user.unlock_achievement(new/datum/achievement/miss())
 
 	if(special)//We did a special attack, let's apply it's special properties.
-		if(user.atk_intent == I_QUICK)//Faster attack but takes much more stamina.
+		if(user.TALLYHOLADS) //bayonet charge shit
+			var/obj/item/gun/G = user.get_active_hand() //already checked if we have a gun when this is true
+			user.visible_message("<span class='combat_success'>[user] attempts to bayonet [M] with the [G]!</span>")
+			user.adjustStaminaLoss(w_class + 6)
+			user.setClickCooldown(DEFAULT_ATTACK_COOLDOWN)
+			apply_speed_delay(0)
+		else if(user.atk_intent == I_QUICK)//Faster attack but takes much more stamina.
 			user.visible_message("<span class='combat_success'>[user] performs a quick attack!</span>")
 			user.adjustStaminaLoss(w_class + 6)
 			user.setClickCooldown(DEFAULT_QUICK_COOLDOWN)

code/modules/mob/living/carbon/human/human_defense.dm
@@ -251,11 +251,14 @@ meteor_act
 		effective_force *= strToDamageModifier(user.STAT_LEVEL(str))
 
 	if(special)
-		switch(user.atk_intent)
-			if(I_STRONG)//Offensive attacks do even more damage.
-				effective_force += I.force
-			if(I_WEAK)
-				effective_force = (effective_force/2) //Half the amount of force.
+		if(user.TALLYHOLADS) //bayonet stab addition
+			effective_force += user.STAT_LEVEL(str) //putting your *all* into this
+		else
+			switch(user.atk_intent)
+				if(I_STRONG)//Offensive attacks do even more damage.
+					effective_force += I.force
+				if(I_WEAK)
+					effective_force = (effective_force/2) //Half the amount of force.
 
 	if(user.atk_intent == I_GUARD && user.combat_mode)//If we're guarding then hit for less damage.
 		effective_force = (effective_force * 0.25)

Cosmic-Overlord/IS12-Warfare-Two-RU

code/_onclick/item_attack.dm
@@ -94,7 +94,13 @@ avoid code duplication. This includes items that may sometimes act as a standard
 		user.unlock_achievement(new/datum/achievement/miss())
 
 	if(special)//We did a special attack, let's apply it's special properties.
-		if(user.a_intent == I_DISARM)//Faster attack but takes much more stamina.
+		if(user.TALLYHOLADS) //bayonet charge shit
+			var/obj/item/gun/G = user.get_active_hand() //already checked if we have a gun when this is true
+			user.visible_message("<span class='combat_success'>[user] attempts to bayonet [M] with the [G]!</span>")
+			user.adjustStaminaLoss(w_class + 6)
+			user.setClickCooldown(DEFAULT_ATTACK_COOLDOWN)
+			apply_speed_delay(0)
+		else if(user.a_intent == I_DISARM)//Faster attack but takes much more stamina.
 			user.visible_message("<span class='combat_success'>[user] performs a quick attack!</span>")
 			user.adjustStaminaLoss(w_class * 6) //increased stamina loss the bigger the item is
 			user.setClickCooldown(DEFAULT_QUICK_COOLDOWN)

code/modules/mob/living/carbon/human/human_defense.dm
@@ -259,11 +259,14 @@ meteor_act
 		effective_force *= strToDamageModifier(user.STAT_LEVEL(str))
 
 	if(special)
-		switch(user.a_intent)
-			if(I_HURT)//Offensive attacks do even more damage.
-				effective_force += I.force
-			if(I_WEAK)
-				effective_force = (effective_force/2) //Half the amount of force.
+		if(user.TALLYHOLADS) //bayonet stab addition
+			effective_force += user.STAT_LEVEL(str) //putting your *all* into this
+		else
+			switch(user.a_intent)
+				if(I_HURT)//Offensive attacks do even more damage.
+					effective_force += I.force
+				if(I_HELP)
+					effective_force = (effective_force/2) //Half the amount of force.
 
 	if(user.a_intent == I_DISARM)//If we're trying to disarm then hit for less damage.
 		effective_force = (effective_force * 0.25)